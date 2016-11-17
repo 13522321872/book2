1c054c7fd4b506c20989036ede3b8fb233893a68
seawalkermiaoer <seawalkerxh@gmail.com>
2016-11-17 16:21:35 +0800

    ch03 classification

--- rocket/src/main/scala/com/koala/ch03/AppClassification.scala
+++ rocket/src/main/scala/com/koala/ch03/AppClassification.scala
@@ -1,40 +1,56 @@
 package com.koala.ch03
 
+import org.apache.log4j.{Level, Logger}
 import org.apache.spark.ml.classification.NaiveBayes
 import org.apache.spark.ml.evaluation.MulticlassClassificationEvaluator
 import org.apache.spark.sql.SparkSession
 
 /**
-  * Created by haixiang on 2016/9/24.
+  * Created by seawalker on 2016/9/24.
+  *
   */
 
 object AppClassification {
 
   def main(args: Array[String]): Unit = {
+    Logger.getLogger("org.apache.spark").setLevel(Level.WARN)
+    /**
+      * whdir: omitted.
+      * input: libsvm file.
+      * output: model store path.
+      * mode: yarn-client, yarn-cluster or local[*].
+      */
+    val Array(whdir, input, output, mode) = args
     val spark = SparkSession
       .builder
-      .config("spark.sql.warehouse.dir", "file:///E:/git/spark-warehouse")
-      .master("local[4]")
+      .config("spark.sql.warehouse.dir", whdir)
+      .master(mode)
       .appName(this.getClass.getName)
       .getOrCreate()
 
-
-    // Load the data stored in LIBSVM format as a DataFrame.
-    val data = spark.read.format("libsvm").load("E:/git/data/ch02/stage02")
-    // Split the data into training and test sets (30% held out for testing)
+    val data = spark.read.format("libsvm").load(input)
     val Array(trainingData, testData) = data.randomSplit(Array(0.7, 0.3), seed = 1234L)
+
     // Train a NaiveBayes model.
     val model = new NaiveBayes().fit(trainingData)
-    // Select example rows to display.
     val predictions = model.transform(testData)
-    predictions.show()
-    // Select (prediction, true label) and compute test error
     val evaluator = new MulticlassClassificationEvaluator()
       .setLabelCol("label")
       .setPredictionCol("prediction")
       .setMetricName("accuracy")
+
     val accuracy = evaluator.evaluate(predictions)
     println("Accuracy: " + accuracy)
+
+    /**
+ |-- label: double (nullable = true)
+ |-- features: vector (nullable = true)
+ |-- rawPrediction: vector (nullable = true)
+ |-- probability: vector (nullable = true)
+ |-- prediction: double (nullable = true)
+      */
+    model.save(output)
     spark.stop()
   }
+
 }
