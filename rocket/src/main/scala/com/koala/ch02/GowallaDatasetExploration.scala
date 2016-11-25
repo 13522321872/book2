472ae71a032e11ff82ea55eb447cba05b0e8df98
seawalkermiaoer <seawalkerxh@gmail.com>
2016-11-25 15:34:47 +0800

    ch02 bug fixed

--- rocket/src/main/scala/com/koala/ch02/GowallaDatasetExploration.scala
+++ rocket/src/main/scala/com/koala/ch02/GowallaDatasetExploration.scala
@@ -1,11 +1,10 @@
 package com.koala.ch02
 
-import org.apache.log4j.{Logger,Level}
+import org.apache.log4j.{Level, Logger}
+import org.apache.spark.mllib.linalg.{Vectors, _}
 import org.apache.spark.mllib.stat.{MultivariateStatisticalSummary, Statistics}
-import org.apache.spark.mllib.linalg.Vectors
 import org.apache.spark.{SparkConf, SparkContext}
 import org.joda.time.format.DateTimeFormat
-import org.joda.time.{DateTime, LocalDate}
 ;
 /**
  * Created by haixiang on 2016/4/9.
@@ -15,40 +14,43 @@ import org.joda.time.{DateTime, LocalDate}
  * size: 6442892
  * user: 107092
  */
-case class CheckIn(user: String, time: DateTime, latitude: Double, longitude: Double, location: String)
+case class CheckIn(user: String, time: String, latitude: Double, longitude: Double, location: String)
 
 object GowallaDatasetExploration {
 
   def main(args: Array[String]) {
     Logger.getLogger("org.apache.spark").setLevel(Level.WARN)
+    val Array(mode, input) = args
 
-    val Array(input, mode) = args
     val conf = new SparkConf().setAppName(this.getClass.getSimpleName).setMaster(mode)
     val sc = new SparkContext(conf)
 
     val gowalla = sc.textFile(input).map(_.split("\t")).mapPartitions{
       case iter =>
         val format = DateTimeFormat.forPattern("yyyy-MM-dd\'T\'HH:mm:ss\'Z\'")
-        iter.map{
-        case terms => CheckIn(terms(0), DateTime.parse(terms(1),format), terms(2).toDouble, terms(3).toDouble,terms(4))
+        iter.map {
+          //DateTime.parse(terms(1),format)
+        case terms => CheckIn(terms(0), terms(1).substring(0, 10), terms(2).toDouble, terms(3).toDouble,terms(4))
       }
     }
 
     //user, check ins, check in days, locations
     val observations = gowalla.map{
-      case check => (check.user, (1L, Set(check.time.toLocalDate), Set(check.location)))
-    }.reduceByKey{
+      case check: CheckIn => (check.user, (1L, Set(check.time), Set(check.location)))
+    }.reduceByKey {
       case (left, right) => (left._1 + right._1, left._2.union(right._2),  left._3.union(right._3))
-    }.map{
-      case (user, (checkins, days:Set[LocalDate], locations:Set[String])) =>
+    }.map {
+      case (user, (checkins, days:Set[String], locations:Set[String])) =>
         Vectors.dense(checkins.toDouble, days.size.toDouble, locations.size.toDouble)
     }
-
 
     val summary: MultivariateStatisticalSummary = Statistics.colStats(observations)
     println(summary.mean)
     println(summary.variance)
     println(summary.numNonzeros)
+
+    val correlMatrix: Matrix = Statistics.corr(observations, "pearson")
+    println(correlMatrix.toString)
 
     sc.stop()
 
